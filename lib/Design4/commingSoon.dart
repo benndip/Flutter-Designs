13a0b4e75877d28298b5e4e96bbcffac4e55b202
benndip <obentabiayuk1@gmail.com>
2021-04-23 17:54:04 +0100

    Still Designing Fisherman's diary

--- lib/Design4/commingSoon.dart
+++ lib/Design4/commingSoon.dart
@@ -14,7 +14,6 @@ class _CommingSoonState extends State<CommingSoon> {
       child: Container(
         padding: EdgeInsets.all(0),
         width: deviceWidth,
-        // height: deviceHeight,
         child: Column(
           children: [
             Container(
@@ -36,12 +35,6 @@ class _CommingSoonState extends State<CommingSoon> {
                       child: Container(
                         height: 60,
                         width: deviceWidth * 0.71,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                            color: Colors.red,
-                          ),
-                        ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -77,7 +70,7 @@ class _CommingSoonState extends State<CommingSoon> {
                                   Container(
                                     margin: EdgeInsets.only(left: 3),
                                     child: Text(
-                                      '2013',
+                                      '2020',
                                       style: TextStyle(
                                           color: Colors.white70, fontSize: 10),
                                     ),
@@ -96,7 +89,7 @@ class _CommingSoonState extends State<CommingSoon> {
                                   Container(
                                     margin: EdgeInsets.only(left: 3),
                                     child: Text(
-                                      '2h 6mins',
+                                      '2h 23mins',
                                       style: TextStyle(
                                           color: Colors.white70, fontSize: 12),
                                     ),
@@ -112,12 +105,14 @@ class _CommingSoonState extends State<CommingSoon> {
                                 decoration: BoxDecoration(
                                   color: Colors.black87,
                                   borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(
+                                      width: 0.3, color: Colors.grey),
                                 ),
                                 child: Center(
                                   child: Icon(
                                     Icons.chevron_left,
                                     color: Colors.white70,
-                                    size: 25,
+                                    size: 35,
                                   ),
                                 ),
                               ),
@@ -130,139 +125,213 @@ class _CommingSoonState extends State<CommingSoon> {
                 ],
               ),
             ),
-            Container(
-              width: deviceWidth,
-              padding: EdgeInsets.all(25),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.red,
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Fisherman\'s Diary',
-                    style: TextStyle(
-                        color: Colors.white60,
-                        fontSize: 27,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Enah Johnscott',
-                    style: TextStyle(
-                      color: Colors.white60,
-                      fontSize: 11,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
+            Stack(
+              children: [
+                Container(
+                  width: deviceWidth,
+                  padding: EdgeInsets.all(25),
+                  child: Row(
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        height: 25,
-                        padding: EdgeInsets.all(5),
-                        child: Center(
-                          child: Text(
-                            'Romantic',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Fisherman\'s Diary',
+                            style: TextStyle(
+                                color: Colors.white60,
+                                fontSize: 27,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Enah Johnscott',
                             style: TextStyle(
                               color: Colors.white60,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w200,
+                              fontSize: 11,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.black12,
-                          border: Border.all(width: 0.4, color: Colors.grey),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        padding: EdgeInsets.all(5),
-                        height: 25,
-                        // width: 70,
-                        child: Center(
-                          child: Text(
-                            'Drama',
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 10),
+                                height: 25,
+                                padding: EdgeInsets.all(5),
+                                child: Center(
+                                  child: Text(
+                                    'Educational',
+                                    style: TextStyle(
+                                      color: Colors.white60,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w200,
+                                    ),
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  border: Border.all(
+                                      width: 0.4, color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.all(5),
+                                height: 25,
+                                // width: 70,
+                                child: Center(
+                                  child: Text(
+                                    'Drama',
+                                    style: TextStyle(
+                                      color: Colors.white60,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w200,
+                                    ),
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  border: Border.all(
+                                      width: 0.4, color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.all(5),
+                                height: 25,
+                                // width: 70,
+                                child: Center(
+                                  child: Text(
+                                    'African',
+                                    style: TextStyle(
+                                      color: Colors.white60,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w200,
+                                    ),
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  border: Border.all(
+                                    width: 0.4,
+                                    color: Colors.grey,
+                                  ),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: deviceWidth * 0.50,
+                            height: 0.2,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Introduction',
                             style: TextStyle(
                               color: Colors.white60,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w200,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.black12,
-                          border: Border.all(width: 0.4, color: Colors.grey),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        padding: EdgeInsets.all(5),
-                        height: 25,
-                        // width: 70,
-                        child: Center(
-                          child: Text(
-                            'Educational',
-                            style: TextStyle(
-                              color: Colors.white60,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w200,
-                            ),
+                          SizedBox(
+                            height: 10,
                           ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.black12,
-                          border: Border.all(width: 0.4, color: Colors.grey),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
+                          Container(
+                            width: deviceWidth * 0.50,
+                            child: Text(
+                              'A 12-year-old girl fights to go to school in a village where female education is considered taboo.Ekah is determined to go to school in a village of fishermen where a girl child\'s education is considered a taboo. Her drive to break this old adage gets her embroiled with her father, Solomon\'s past.',
+                              style: TextStyle(
+                                color: Colors.white60,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w200,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: deviceWidth * 0.50,
-                    height: 0.2,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Introduction',
-                    style: TextStyle(
-                      color: Colors.white60,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                ),
+                Positioned(
+                  right: 30,
+                  top: 30,
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      top: 5,
+                    ),
+                    width: 50,
+                    height: deviceHeight * 0.35,
+                    child: Column(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.keyboard_arrow_up_outlined,
+                              color: Colors.grey,
+                              size: 20,
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_up_outlined,
+                              color: Colors.grey[800],
+                              size: 20,
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_up_outlined,
+                              color: Colors.grey[900],
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 80,
+                          margin: EdgeInsets.only(top: 63),
+                          width: double.infinity,
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: Center(
+                              child: Text(
+                                'BOOk NOW',
+                                style: TextStyle(
+                                  color: Colors.white60,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 0.4,
+                            ),
+                            color: Color.fromRGBO(255, 19, 4, 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 0.4,
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    width: deviceWidth * 0.50,
-                    child: Text(
-                      'A 12-year-old girl fights to go to school in a village where female education is considered taboo.Ekah is determined to go to school in a village of fishermen where a girl child\'s education is considered a taboo. Her drive to break this old adage gets her embroiled with her father, Solomon\'s past.',
-                      style: TextStyle(
-                          color: Colors.white60,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w200),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             )
           ],
         ),
